7013098a662c8c732f152217054b29230fa378ae
Muhammad Amran Khan (Mike) <Tipu870@gmail.com>
2023-09-07 18:08:44 +0500

    Update solver.rb

--- solver.rb
+++ solver.rb
@@ -8,4 +8,20 @@ class Solver
       num * factorial(num - 1)
     end
   end
+
+  def reverse(word)
+    word.reverse
+  end
+
+  def fizzbuzz(n)
+    if n % 3 == 0 && n % 5 == 0
+      'fizzbuzz'
+    elsif n % 3 == 0
+      'fizz'
+    elsif n % 5 == 0
+      'buzz'
+    else
+      n.to_s
+    end
+  end
 end
