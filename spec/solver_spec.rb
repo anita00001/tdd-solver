21690b6cae57ab4fea674d121a203247e554f39f
Muhammad Amran Khan (Mike) <Tipu870@gmail.com>
2023-09-07 18:03:41 +0500

    Update solver_spec.rb with TDD

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -22,9 +22,36 @@ describe Solver do
       expect { @solver.factorial(-1) }.to raise_error(ArgumentError)
     end
   end
-  # describe 'Reverse String method' do
-  # end
 
-  # describe 'FizzBuzz method' do
-  # end
+  describe 'Reverse String method' do
+    it 'reverses a string' do
+      expect(@solver.reverse('hello')).to eq('olleh')
+    end
+
+    it 'returns an empty string for an empty input' do
+      expect(@solver.reverse('')).to eq('')
+    end
+
+    it 'reverses a string with spaces' do
+      expect(@solver.reverse('hello world')).to eq('dlrow olleh')
+    end
+  end
+
+  describe 'FizzBuzz method' do
+    it 'returns "fizz" when N is divisible by 3' do
+      expect(@solver.fizzbuzz(9)).to eq('fizz')
+    end
+
+    it 'returns "buzz" when N is divisible by 5' do
+      expect(@solver.fizzbuzz(10)).to eq('buzz')
+    end
+
+    it 'returns "fizzbuzz" when N is divisible by both 3 and 5' do
+      expect(@solver.fizzbuzz(15)).to eq('fizzbuzz')
+    end
+
+    it 'returns N as a string for any other case' do
+      expect(@solver.fizzbuzz(7)).to eq('7')
+    end
+  end
 end
