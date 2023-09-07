18c316a4023e898a6de654093e540b89d1d6c418
Anita Sharma <sharma.anita00001@gmail.com>
2023-09-07 12:35:56 +0545

    Test cases for Factorial method

---
+++ spec/solver_spec.rb
@@ -0,0 +1,23 @@
+require_relative '../solver'
+
+describe Solver do
+  @solver = Solver.new
+
+  describe "Factorial method" do
+    it "Factorial of 5 should be equal to 120" do
+      expect(@solver.factorial(5)).to eq(120)
+    end
+
+    it "Factorial of 1 should be equal to 1" do
+      expect(@solver.factorial(1)).to eq(1)
+    end
+
+    it "Factorial of 0 should be equal to 1" do
+      expect(@solver.factorial(0)).to eq(1)
+    end
+
+    it "Factorial of negative number should raise an exception" do
+      expect { @solver.factorial(-1) }.to raise_error(ArgumentError)
+    end
+  end
+end
